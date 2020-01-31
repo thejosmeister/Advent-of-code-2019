95702c784c0b87e7dfa7688b6e5f808940c01f5e
thejosmeister <60488435+thejosmeister@users.noreply.github.com>
2020-01-31 15:10:08 +0000

    Update Day6.java

--- Day6.java
+++ Day6.java
@@ -6,9 +6,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-/**
- * @author josiah.filleul
- */
 public class Day6
 {
     //    public static String input = "COM)B\n"
