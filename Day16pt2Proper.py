d6463c7fe1a0aa2712ec43daf02fec59bc4274dc
thejosmeister <60488435+thejosmeister@users.noreply.github.com>
2020-02-14 16:37:26 +0000

    Finally done this

--- Day16pt2Proper.py
+++ Day16pt2Proper.py
@@ -9,7 +9,7 @@
 # 804 * 650 = 522600 so need the 41 rear most digits of ans on front of 804 lots of the input.
 
 
-import time
+
 
 with open("day16Input.txt") as f:
     file_input = f.readlines()
@@ -18,7 +18,7 @@ input_string1 = file_input[0]
 input_string1 = input_string1.strip()
 
 # input_string1 = "03036732577212944063491565474664"
-# input_string = "123456789123456789"
+# input_string = "123456789123456789123456789123456789"
 
 input_list = []
 
@@ -34,47 +34,72 @@ length_of_input = len(input_string)
 for i in range(length_of_input):
     input_list.append(int(input_string[i]))
 
-# input has been created so now want to implement a recursion formula to find digits at phase 100.
+# Input has been created so now want to implement a recursion formula to find digits at phase 100.
+
+# Original recursion formula didn't work as I was incorrectly using patterns observed when raising
+# the corresponding matrix to the power of 100.
+# 2nd attempt using find_digit function also didn't work as I reached recursion limit for python.
+# Finally just used the same recursion but with a huge dictionary to hold values which worked.
+# This would have taken loads less time if I had read the question properly.
+
+
 print("input list created")
 print(length_of_input)
 no_of_phases = 0
+dict_of_values = {}
+
+for i in range(length_of_input):
+    dict_of_values[str([i, 0])] = input_list[i]
+
+a = length_of_input - 1
+last_entry = input_list[a]
+for i in range(1, 101):
+    dict_of_values[str([a, i])] = last_entry
+
+for i in range(1, 101):
+    for j in range(length_of_input-2, -1, -1):
+        dict_of_values[str([j, i])] = int(str(dict_of_values[str([j + 1, i])] + dict_of_values[str([j, i - 1])])[-1])
 
 
-def find_digit(index: int) -> int:
-    global input_list
-    output = input_list[index]
-    counter = 4 + index
-    while counter < length_of_input:
-        # if length_of_input - counter < 4:
-        #     if (counter - index) % 4 == 2:
-        #         counter = counter + 2
-        #     else:
-        #         output = output + input_list[counter]*5
-        #         counter = counter + 1
-        # else:
-            output = output + input_list[counter]*5
-            counter = counter + 4
-
-    return int(str(output)[-1])
+print(dict_of_values['[0, 100]'])
+print(dict_of_values['[1, 100]'])
+print(dict_of_values['[2, 100]'])
+print(dict_of_values['[3, 100]'])
+print(dict_of_values['[4, 100]'])
+print(dict_of_values['[5, 100]'])
+print(dict_of_values['[6, 100]'])
+print(dict_of_values['[7, 100]'])
 
 
-a = find_digit(0)
-b = find_digit(1)
-c = find_digit(2)
-d = find_digit(3)
-e = find_digit(4)
-f = find_digit(5)
-g = find_digit(6)
-h = find_digit(7)
+
+# def find_digit(index: int, power: int) -> int:
+#     global input_list
+#     global dict_of_values
+#     print(str(index) + ", " + str(power))
+#     if power == 0:
+#         return input_list[index]
+#     elif index == length_of_input - 1:
+#         return input_list[-1]
+#     else:
+#         dict_constant = str(index) + "," + str(power)
+#         if dict_constant in dict_of_values:
+#             return dict_of_values[dict_constant]
+#         else:
+#             interim = int(str(find_digit(index + 1, power) + find_digit(index, power - 1))[-1])
+#             dict_of_values[dict_constant] = interim
+#             if power == 100:
+#                 if index < 8:
+#                     print(interim)
+#             return int(str(find_digit(index + 1, power) + find_digit(index, power - 1))[-1])
 #
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
-print(f)
-print(g)
-print(h)
+#
+# find_digit(0, 1)
+
+
+# a = find_digit(0, 100)
+
+#
+# print(a)
 
 
 # 85798498 is too high
@@ -101,4 +126,3 @@ print(h)
 #     phases = phases + 1
 #
 # print(input_list)
-
